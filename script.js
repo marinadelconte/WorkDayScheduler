eef070e0a852d3cf1c7e049db8795fb25e8e09b1
Marina Del Conte <marinadelconte@yahoo.com>
2023-08-02 21:21:39 -0400

    added a README template and created additional calendar time blocks

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ $(function () {
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
+
+  parseInt(document.querySelectorAll(".row")[0].dataset.time) < dayjs().hour()
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
