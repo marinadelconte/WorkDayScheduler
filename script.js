be8d3308d0c9d5df67398845af6012beb3965152
Marina Del Conte <marinadelconte@yahoo.com>
2023-08-04 09:53:11 -0400

    corrected entry and rendering to/from local storage

--- script.js
+++ script.js
@@ -7,6 +7,50 @@ setInterval(function() {
   timeEl.textContent = dayjs().format("dddd, MMMM D [at] hh:mm a")
 
 }, 1000);
+colorTimeBlocks();
+
+for (let i = 0; i < saveButtonsEl.length; i++) {
+  saveButtonsEl[i].addEventListener("click", function(event) {
+   event.preventDefault();
+   
+   
+   
+   console.log(this.getAttribute("data-time"));
+   let entry = document.querySelectorAll(".description").value;
+if (event.target.matches(".saveBtn")) {
+localStorage.setItem(this.getAttribute("data-time"), this.previousElementSibling.value);
+ }
+
+});
+}
+
+calendarEntries[0].value = localStorage.getItem(9)
+console.log(localStorage.getItem(9))
+
+calendarEntries[1].value = localStorage.getItem(10)
+console.log(localStorage.getItem(10))
+
+calendarEntries[2].value = localStorage.getItem(11)
+console.log(localStorage.getItem(11))
+
+calendarEntries[3].value = localStorage.getItem(12)
+console.log(localStorage.getItem(12))
+
+calendarEntries[4].value = localStorage.getItem(13)
+console.log(localStorage.getItem(13))
+
+calendarEntries[5].value = localStorage.getItem(14)
+console.log(localStorage.getItem(14))
+
+calendarEntries[6].value = localStorage.getItem(15)
+console.log(localStorage.getItem(15))
+
+calendarEntries[7].value = localStorage.getItem(16)
+console.log(localStorage.getItem(16))
+
+calendarEntries[8].value = localStorage.getItem(9)
+console.log(localStorage.getItem(16))
+
 
 function colorTimeBlocks(){
   for (let i = 0; i < calendarEntries.length; i++) {
@@ -23,20 +67,7 @@ function colorTimeBlocks(){
     }
   }
 }
-colorTimeBlocks();
 
-for (let i = 0; i < saveButtonsEl.length; i++) {
-   saveButtonsEl[i].addEventListener("click", function() {
-    event.preventDefault();
-    let entry = document.querySelectorAll(".description").value;
-if (event.target.matches(".saveBtn")) {
-localStorage.setItem("text", "this.parentElement.previousSibling.value");
-  }
-});
-}
-
-
-  
 
 // function renderLastEntry() {
 // let calendarEntry = localStorage.getItem(".description");
